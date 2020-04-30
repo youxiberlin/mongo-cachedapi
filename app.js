3b3e8294c3d0bf3cb11d418318beb4c0b56d254f
Yuki Sato <yuki.sato@lightcurve.io>
2020-04-30 17:37:52 +0200

    Add mongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const { port, mongoID, mongoPW } = require('./config');
+const mongoose = require('mongoose');
+const { port, dbRoute } = require('./config');
+
+mongoose.connect(dbRoute, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(() => console.log('connected to mongoDB'),
+	err => console.log(`mongoDB connection error: ${err}`))
 
 const app = express();
 app.get('/', (req, res) => res.send('hello world'));
-app.listen(port, () => console.loggit(`App listening at port ${port}`));
+app.listen(port, () => console.log(`App listening at port ${port}`));
--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 3000;
 const mongoID = process.env.MONGO_ID || '';
 const mongoPW = process.env.MONGO_PW || '';
+const dbRoute = `mongodb+srv://${mongoID}:${mongoPW}@cluster0-xzvas.mongodb.net/test?retryWrites=true&w=majority`;
 
-module.exports = { port, mongoID, mongoPW };
+module.exports = { port, dbRoute };
