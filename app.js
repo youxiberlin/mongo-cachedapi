59defca3484ddff270c3d5d7416fa9e0f964b3d8
Yuki Sato <yuki.sato@lightcurve.io>
2020-05-01 17:19:27 +0200

    Add body-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const routes = require('./routes');
@@ -9,10 +10,14 @@ const { port, dbRoute } = require('./config');
 mongoose.connect(dbRoute, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
+	useFindAndModify: false
 }).then(() => console.log('connected to mongoDB'),
 	err => console.log(`mongoDB connection error: ${err}`))
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use('/data', routes);
 app.get('/', (req, res) => res.send('hello world'));
 app.listen(port, () => console.log(`App listening at port ${port}`));
