9c209b8099c1f63a4484d8ca61deecf10b90598e
Yuki Sato <yuki.sato@lightcurve.io>
2020-05-04 20:16:59 +0200

    Change mongodb config

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const port = process.env.PORT || 3000;
 const mongodb = {
 	host: process.env.MONGODB_HOST || 'localhost',
 	port: process.env.MONGODB_PORT || 27017,
-	db: 'test',
+	db: 'cacheapi',
 	user: process.env.MONGO_ID || '',// optional
 	password: process.env.MONGO_PW || '', // optional
 };
 const mongoRoute = `mongodb://${mongodb.host}:${mongodb.port}/${mongodb.db}`
-const mongoClusterRoute = `mongodb+srv://${mongoID}:${mongoPW}@cluster0-xzvas.mongodb.net/test?retryWrites=true&w=majority`;
+const mongoClusterRoute = `mongodb+srv://${mongodb.user}:${mongodb.password}@cluster0-xzvas.mongodb.net/test?retryWrites=true&w=majority`;
 const dbCacheLimit = 100;
 const ttlSeconds = 3600;
 
-module.exports = { port, dbRoute, dbCacheLimit, ttlSeconds, mongoRoute };
+module.exports = { port, dbCacheLimit, ttlSeconds, mongoRoute };
