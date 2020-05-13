a76d0a60a2b13a1f0281e0fe9c876cc71220768f
Yuki Sato <yuki.sato@lightcurve.io>
2020-05-13 16:47:04 +0200

    Refactor validator

--- services/validator.js
+++ services/validator.js
@@ -1,14 +1,21 @@
-const validate = (obj, def) => {
+const logger = require('./logger');
+
+const validate = (obj, def, errs = []) => {
 	const keys = Object.keys(obj);
-	const errs = [];
+	const isArray = obj => Array.isArray(obj);
+	const validateObj = key => typeof obj[key] === def[key];
 
 	keys.forEach((key) => {
-		const isTypeValid = typeof obj[key] === def[key];
-		if (!isTypeValid) errs.push(`invalid type of data: ${obj[key]} is not ${def[key]} `);
-	})
+		if (isArray(obj[key])) {
+			if (!isArray(def[key])) errs.push(`invalid type of data: ${key} is not Array`);
+			else return validate(obj[key][0], def[key][0], errs);
+		} else {
+			if (!validateObj(key)) errs.push(`invalid type of data: ${obj[key]} is not ${def[key]} `);
+		}
+	});
 	
 	if (errs.length) {
-		errs.forEach(err => console.error(err))
+		errs.forEach(err => logger.error(err))
 	}
 	return errs.length ? false : true;
 };
